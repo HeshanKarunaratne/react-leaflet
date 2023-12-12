ea3a68f923eb694fb545d8e93cd7cb9e83d9d374
HeshanKarunaratne <heshan947102@gmail.com>
2023-12-12 20:41:34 +0530

    added key to mapcontainer

--- src/App.js
+++ src/App.js
@@ -7,20 +7,21 @@ import { useState } from 'react';
 function App() {
 
   const locations = [
-    { id: 1, name: 'Location 1', lat: 47.7749, lng: -122.4194 },
-    { id: 2, name: 'Location 2', lat: 24.0522, lng: -118.2437 },
-
+    { id: 1, name: 'Location 1', lat: 30.7749, lng: -12.4194 },
+    { id: 2, name: 'Location 2', lat: 35.0522, lng: -115.2437 },
   ];
 
   const [mapCenter, setMapCenter] = useState([35.7749, -120.4194]);
   const [mapZoom, setMapZoom] = useState(5);
 
-  const handleMarkerClick = async (lat, lng) => {
+  const handleMarkerClick = (lat, lng) => {
     console.log("clicked in ", lat, lng)
-    await setMapCenter([lat, lng]);
-    await setMapZoom(13);
+    setMapZoom(13);
+    setMapCenter([lat, lng]);
   };
 
+  console.log("zoom:", mapZoom);
+  console.log("center:", mapCenter)
   return (
     <>
       <div>
@@ -33,22 +34,18 @@ function App() {
           ))}
         </ul>
       </div>
-      <MapContainer center={mapCenter} zoom={mapZoom}>
+      <MapContainer key={`${mapZoom}-${mapCenter[0]}-${mapCenter[1]}`} center={mapCenter} zoom={mapZoom}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
         {locations.map((location) => (
           <Marker key={location.id} position={[location.lat, location.lng]}>
             <Popup>{location.name}</Popup>
           </Marker>
         ))}
-
       </MapContainer>
     </>
-
   );
 }
-
 export default App;
